02f19d344aff48e2e76642caae948a898831f72e
Marwan-98 <marwanashrafzaki98@gmail.com>
2024-06-07 19:06:05 +0300

    Implement logic to fix the date format if it's not correct

--- src/app/api/certificate/route.config.ts
+++ src/app/api/certificate/route.config.ts
@@ -1,7 +1,10 @@
+import { format } from "date-fns";
+
 export function extractDataFromFileName(str: string) {
   // Regular expressions to extract the parts
   const idRegex = /^(\d+)/;
   const dateRegex = /\(([^)]+)\)/;
+  const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
 
   // Extract the first part using match
   const idMatch = str.match(idRegex);
@@ -10,6 +13,20 @@ export function extractDataFromFileName(str: string) {
   // Extract the date part using match
   const dateMatch = str.match(dateRegex);
   const date = dateMatch ? dateMatch[1] : null;
+
+  if (date && dateFormatRegex.test(date)) {
+    return { id, date };
+  } else if (date && !dateFormatRegex.test(date)) {
+    const dateComponents = date.split("-");
+    const day = dateComponents[0];
+    const month = dateComponents[1];
+    const year = dateComponents[2];
+
+    // Reformatting the date
+    const reformattedDate = format(`${year}-${month}-${day}`, "yyyy-MM-dd");
+
+    return { id, date: reformattedDate };
+  }
 
   return { id, date };
 }
